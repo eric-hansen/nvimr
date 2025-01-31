b53ec76e5ad2b394d4d2bcafd503c19302641af9
Eric Hansen <eric@thirteentwo.co>
2025-01-31 08:45:34 -0500

    some more helpers in utils lib.  mainly a func to render a centered window

--- lua/utils.lua
+++ lua/utils.lua
@@ -2,12 +2,24 @@ local keymap = vim.keymap
 
 M = {}
 
-M.create_keymap = function (modes, key_combo, exec, desc)
-	opts = {}
-	
-	if desc ~= nil then opts.desc = desc end
+M.create_keymap = function(modes, key_combo, exec, desc, opts)
+	local _opts = vim.tbl_deep_extend("force", {}, opts or {})
 
-	keymap.set(modes, key_combo, exec, opts)
+	if desc ~= nil then _opts.desc = desc end
+
+	keymap.set(modes, key_combo, exec, _opts)
+end
+
+M.center_window = function()
+	local height = math.floor(0.618 * vim.o.lines)
+	local width = math.floor(0.618 * vim.o.columns)
+	return {
+		anchor = 'NW',
+		height = height,
+		width = width,
+		row = math.floor(0.5 * (vim.o.lines - height)),
+		col = math.floor(0.5 * (vim.o.columns - width)),
+	}
 end
 
 return M
