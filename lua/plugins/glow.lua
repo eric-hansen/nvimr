da989b8a79a8049f68abd1831d2b1497ea402451
Eric Hansen <eric@thirteentwo.co>
2025-02-05 13:37:39 -0500

    only try to set up glow() if we can load a file, word

--- lua/plugins/glow.lua
+++ lua/plugins/glow.lua
@@ -1,8 +1,11 @@
-if vim.fn.executable("glow") ~= 0 then
-    require("glow").setup({
-        style = "dark",
-        default_type = "preview",
-    })
+local utils = require('utils')
+local glow = utils.preq('glow')
+
+if vim.fn.executable("glow") ~= 0 and glow ~= nil then
+	glow.setup({
+		style = "dark",
+		default_type = "preview",
+	})
 else
-    vim.notify("glow needs to be installed first!", vim.log.levels.ERROR)
+	vim.notify("glow needs to be installed first!", vim.log.levels.ERROR)
 end
